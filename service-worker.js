208c5dcad660a3034a41c286ce4153bdd88d5d18
Sarah Gebellini <162918062+SarahGebellini@users.noreply.github.com>
2025-10-29 17:34:50 -0300

    Fix quotes in service worker file

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,17 @@
-const CACHE_NAME = "meu-pwa-cache-v1";
-const urlsToCache = ["/index.html", "/script.js", "/style.css"];
+const CACHE_NAME = 'meu-pwa-cache-v1';
+const urlsToCache = ['/index.html', '/script.js', '/style.css'];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches
       .match(event.request)
       .then((response) => response || fetch(event.request))
   );
 });
+
